e366a9018325f6e9f076591c341bb1d012ed488f
PeterJochem <peterjochem2020@u.northwestern.edu>
2021-10-03 18:20:20 -0400

    Add Express server endpoints

--- server/app.js
+++ server/app.js
@@ -1,28 +1,81 @@
 const http = require('http');
 var mysql = require('mysql');
-
+const express = require('express')
+const app = express()
+const port = 3001
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "peter",
-  password: "password"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+  user: "root",
+  password: "delicious"
 });
 
 
-const hostname = '127.0.0.1';
-const port = 3001;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({"a": 1}));
+function read_to_dos() {
+return new Promise(function(resolve, reject){
+	con.query("SELECT Name, Description FROM to_do.to_dos", (err, rows) => {
+  	if (err) throw err;
+	resolve(rows);
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
 });
+}
+
+function delete_to_do(name) {
+return new Promise(function(resolve, reject){
+	con.query("DELETE FROM to_do.to_dos WHERE name='" + name + "';", (err, rows) => {
+        if (err) throw err;
+        resolve(rows);
+});
+});
+}
+
+
+function add_to_do(name, description, priority=null, date_entered=null, date_finished=null) {
+	return new Promise(function(resolve, reject){
+		let str = "INSERT INTO to_do.to_dos VALUES ('" + name + "', '" + description + "', " + "" + priority + ", ";
+		str = str + "" + date_entered + ", " + "" + date_finished + "" + ");" 
+	        //console.log(str);
+		con.query(str, (err, rows) => {
+        	if (err) throw err;
+		resolve(rows);
+		});
+	});
+}
+
+
+app.get('/read_to_dos', (req, res) => {
+	try {
+		var promise = read_to_dos();
+		promise.then(function(rows) {
+			res.send(JSON.stringify(rows));
+		})
+	}
+	catch (err) { 
+		console.log("Unable to read to_do entries");
+	}
+})
+
+
+app.get('/add_to_dos', (req, res) => {
+	try { 
+		add_to_do("A new to do entry", "something to do");
+	}
+	catch (err) { 
+		console.log("Unable to add to_do entry with the name " + req.params.name);
+	}
+})
+
+
+app.get('/remove_to_dos/:name', (req, res) => {
+	try {
+		delete_to_do(req.params.name);
+	}
+	catch (err) { 
+		console.log("Unable to delete to_do with the name " + req.params.name);
+	}
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
